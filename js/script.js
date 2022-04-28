255a3eed1486d48178aca5389c7d1fa2dfd27f24
j4vlon <fedot.pushkin98@gmail.com>
2022-04-29 01:30:31 +0500

    create vacansy page

--- js/script.js
+++ js/script.js
@@ -19,6 +19,22 @@
         });
     });
 
+    $(".js-faq-title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("faq__active")) {
+			$(".js-faq-content").slideUp(800);
+			$(".js-faq-title").removeClass("faq__active");
+			$('.js-faq-rotate').removeClass('faq__rotate');
+            
+		}
+        $this.toggleClass("active-v");
+		$this.toggleClass("faq__active");
+		$this.next().slideToggle();
+		$('.js-faq-rotate',this).toggleClass('faq__rotate');
+	});
 
     document.addEventListener("DOMContentLoaded", () => {
         AOS.init({
