62a86ea0041868bd4d0e902afe9012c2c70b3adc
j4vlon <fedot.pushkin98@gmail.com>
2022-04-28 23:10:53 +0500

    chekc git

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ $(".article-txt").hover(function () {
     $(this).toggleClass("article-news-txt");
     $(".article-news-p").toggleClass("un-active");
  });
+ $(function() {
+	
+	//BEGIN
+	$(".js-faq-title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("faq__active")) {
+			$(".js-faq-content").slideUp(800);
+			$(".js-faq-title").removeClass("faq__active");
+			$('.js-faq-rotate').removeClass('faq__rotate');
+		}
+
+		$this.toggleClass("faq__active");
+		$this.next().slideToggle();
+		$('.js-faq-rotate',this).toggleClass('faq__rotate');
+	});
+	//END
+	
+});
